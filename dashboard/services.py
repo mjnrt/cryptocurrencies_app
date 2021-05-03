d19769556359010db18cf006fb5b11f1b1eb8386
Patryk Mejnartowicz <patryk_mej@o2.pl>
2021-05-03 15:38:49 +0200

    1.2_Api/Get historical prices in data range v2

--- dashboard/services.py
+++ dashboard/services.py
@@ -5,8 +5,9 @@ from datetime import datetime, timezone
 
 def my_historical_prices_api(from_time, to_time, interval):
     if to_time == "now":
-        to_time = datetime.now()
-    to_timestamp = str(round(datetime.timestamp(datetime.strptime(to_time, "%Y-%m-%d %H:%M:%S"))))+"000"
+        to_timestamp = str(round(datetime.timestamp(datetime.now()))) + "000"
+    else:
+        to_timestamp = str(round(datetime.timestamp(datetime.strptime(to_time, "%Y-%m-%d %H:%M:%S"))))+"000"
     from_timestamp = str(round(datetime.timestamp(datetime.strptime(from_time, "%Y-%m-%d %H:%M:%S"))))+"000"
 
     url = f"https://api.bitbay.net/rest/trading/candle/history/BTC-PLN/{interval}?from={from_timestamp}&to={to_timestamp}"
@@ -19,4 +20,4 @@ def my_historical_prices_api(from_time, to_time, interval):
     return historical_prices
 
 
-print(my_historical_prices_api('2021-05-03 14:00:00', '2021-05-03 15:30:00', 900))
+print(my_historical_prices_api('2021-05-03 14:00:00', '2021-05-03 14:45:00', 900))
