243872247fb5e6ae34ac6a7fdaf634ed8397415a
Patryk Mejnartowicz <patryk_mej@o2.pl>
2021-05-05 20:23:56 +0200

    creating plots v1

--- dashboard/services.py
+++ dashboard/services.py
@@ -1,16 +1,18 @@
 import requests
 import json
 from datetime import datetime, timezone
+from plotly.offline import plot
+import plotly.graph_objs as go
 
 
-def my_historical_prices_api(from_time, to_time, interval):
+def my_historical_prices_api(from_time, to_time, interval, market, fiat):
     if to_time == "now":
         to_timestamp = str(round(datetime.timestamp(datetime.now()))) + "000"
     else:
-        to_timestamp = str(round(datetime.timestamp(datetime.strptime(to_time, "%Y-%m-%d %H:%M:%S"))))+"000"
-    from_timestamp = str(round(datetime.timestamp(datetime.strptime(from_time, "%Y-%m-%d %H:%M:%S"))))+"000"
+        to_timestamp = str(round(datetime.timestamp(datetime.strptime(to_time, "%Y-%m-%d %H:%M:%S")))) + "000"
+    from_timestamp = str(round(datetime.timestamp(datetime.strptime(from_time, "%Y-%m-%d %H:%M:%S")))) + "000"
 
-    url = f"https://api.bitbay.net/rest/trading/candle/history/BTC-PLN/{interval}?from={from_timestamp}&to={to_timestamp}"
+    url = f"https://api.bitbay.net/rest/trading/candle/history/{market}-{fiat}/{interval}?from={from_timestamp}&to={to_timestamp}"
     response = requests.request("GET", url)
 
     historical_prices = []
@@ -20,4 +22,4 @@ def my_historical_prices_api(from_time, to_time, interval):
     return historical_prices
 
 
-print(my_historical_prices_api('2021-05-03 14:00:00', '2021-05-03 14:45:00', 900))
+# print(my_historical_prices_api('2021-05-03 14:00:00', '2021-05-03 14:45:00', 900))
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from .models import CurrentPrices
 from .cron import my_api_schedule
+from .services import my_historical_prices_api
+from plotly.offline import plot
+from plotly.graph_objs import Scatter
+import plotly.graph_objs as go
 
 
 class LandingPage(View):
@@ -41,5 +45,26 @@ class SingleCryptoPage(View):
 
     def get(self, request, currencie_id):
         currencie = CurrentPrices.objects.get(pk=currencie_id)
-        ctx = {'crypto': currencie}
+        last_year_data = my_historical_prices_api('2021-01-01 00:00:00', 'now', 3600, currencie.market_symbol[0:3],
+                                                  currencie.market_symbol[-3:])
+        ax = []
+        ay = []
+        for single_data in last_year_data:
+            ax.append(str(single_data[0]))
+            ay.append(float(single_data[1]))
+
+        fig = go.Figure()
+        # scatter1 = go.Scatter(x=ax, y=ay,
+        #                       mode='lines', name='open',
+        #                       opacity=0.8, marker_color='green')
+        # fig.add_trace(scatter1)
+        scatter2 = go.Scatter(x=ax, y=ay, mode='lines', name='close', opacity=0.8, marker_color='red')
+        fig.add_trace(scatter2)
+        plot_div = plot(fig, output_type='div')
+        # plot_div = plot([Scatter(x=ax, y=ay,
+        #                          mode='lines', name=currencie.market_symbol,
+        #                          opacity=0.8, marker_color='blue')],
+        #                 output_type='div')
+        ctx = {'crypto': currencie,
+               'plot': plot_div}
         return render(request, self.template_name, ctx)
